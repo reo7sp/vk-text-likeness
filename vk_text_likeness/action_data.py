80bfa3815398cd8eba4f3d245a4f87cd1af0b732
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-04-17 20:10:33 +0300

    Parallel action data

--- vk_text_likeness/action_data.py
+++ vk_text_likeness/action_data.py
@@ -1,7 +1,5 @@
-from functools import lru_cache
-
 import pandas as pd
-from tqdm import tqdm
+from joblib import Parallel, delayed
 
 from vk_text_likeness.logs import log_method_begin, log_method_end
 
@@ -23,29 +21,10 @@ class ActionData:
         log_method_begin()
         print("{} members, {} posts".format(len(self.raw_users_data.members), len(self.raw_wall_data.posts)))
 
+        rows_by_user = Parallel(n_jobs=-1)(delayed(self._compute_user)(user) for user in self.raw_users_data.members)
         rows = []
-
-        friend_post_pairs = set()
-        for user in tqdm(self.raw_users_data.members, 'ActionData.get_all: for members'):
-            if 'groups' not in user:
-                continue
-            for post in self.raw_wall_data.posts:
-                is_liked = user['id'] in post['likes']['user_ids']
-                is_reposted = user['id'] in post['reposts']['user_ids']
-
-                if is_reposted:
-                    for friend in self.raw_users_data.member_friends[user['id']]:
-                        if 'groups' not in friend:
-                            continue
-                        friend_post_pair = (friend['id'], post['id'])
-                        if friend_post_pair not in friend_post_pairs:
-                            friend_is_liked = friend['id'] in post['likes']['user_ids']
-                            friend_is_reposted = friend['id'] in post['reposts']['user_ids']
-
-                            rows.append(self.get_row(friend, post, False, friend_is_liked, friend_is_reposted))
-                            friend_post_pairs.add(friend_post_pair)
-
-                rows.append(self.get_row(user, post, True, is_liked, is_reposted))
+        for it in rows_by_user:
+            rows.extend(it)
 
         result = pd.DataFrame(rows, columns=self.get_labels())
         self.table = result
@@ -58,6 +37,29 @@ class ActionData:
         ))
         log_method_end()
         return result
+
+    def _compute_user(self, user):
+        if 'groups' not in user:
+            return []
+
+        rows = []
+
+        for post in self.raw_wall_data.posts:
+            is_liked = user['id'] in post['likes']['user_ids']
+            is_reposted = user['id'] in post['reposts']['user_ids']
+
+            if is_reposted:
+                for friend in self.raw_users_data.member_friends[user['id']]:
+                    if 'groups' not in friend:
+                        continue
+                    friend_is_liked = friend['id'] in post['likes']['user_ids']
+                    friend_is_reposted = friend['id'] in post['reposts']['user_ids']
+
+                    rows.append(self.get_row(friend, post, False, friend_is_liked, friend_is_reposted))
+
+            rows.append(self.get_row(user, post, True, is_liked, is_reposted))
+
+        return rows
 
     def get_row(self, user, post, is_member, is_liked, is_reposted):
         return [user['id']] + self.table_users_data.get_row(user) + \
